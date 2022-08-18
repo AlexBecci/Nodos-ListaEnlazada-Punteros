f304529a977ba9aca7c71f69e33408a3a363935b
Alex <beccialex2002@gmail.com>
2022-08-18 12:22:31 -0300

    Eliminacion y cortar lista

--- Punteros-Nodos/src/ListaEnlazada.java
+++ Punteros-Nodos/src/ListaEnlazada.java
@@ -33,6 +33,46 @@ public class ListaEnlazada {
         }
         size++;
     }
+    public void cortar(int index)
+    {
+        //1->2->3->...
+        int contador = 0;
+        Nodo temporal = cabeza;
+        while(contador<index-1)
+        {
+            temporal = temporal.obtenerSiguiente();
+            contador++;
+        }
+        temporal.enlazarSiguiente(null);
+        size = index;
+    }
+    public void eliminar(int index)
+    {
+        //1->2->3->4->5->6
+        //1->2->4
+        //1->2->4->5->6
+        if(index ==0)
+        {
+            cabeza = cabeza.obtenerSiguiente();
+        }
+        else
+        {
+            int contador = 0;
+            Nodo temporal = cabeza;
+            while(contador<index-1)
+            {
+                temporal = temporal.obtenerSiguiente();
+                contador++;
+            }
+            temporal.enlazarSiguiente(temporal.obtenerSiguiente().obtenerSiguiente());
+        }
+        size--;
+    }
+    public void eliminarPrimero()
+    {
+        cabeza = cabeza.obtenerSiguiente();
+        size--;
+    }
     public int size(){
         return size;
     }
--- Punteros-Nodos/src/Main.java
+++ Punteros-Nodos/src/Main.java
@@ -2,28 +2,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        Nodo primer = new Nodo("PrimerNodo");
-        Nodo segundo = new Nodo(45);
-        Nodo tercer = new Nodo("Ultimo valor al nodo ingresado ingresado");
-        primer.enlazarSiguiente(segundo);
-        primer.obtenerSiguiente().enlazarSiguiente(tercer);
-        System.out.println(primer.obtenerSiguiente().obtenerSiguiente().obtenerValor().toString());
+//        Nodo primer = new Nodo("PrimerNodo");
+//        Nodo segundo = new Nodo(45);
+//        Nodo tercer = new Nodo("Ultimo valor al nodo ingresado ingresado");
+//        primer.enlazarSiguiente(segundo);
+//        primer.obtenerSiguiente().enlazarSiguiente(tercer);
+//        System.out.println(primer.obtenerSiguiente().obtenerSiguiente().obtenerValor().toString());
+
         //Segunda prueba
-        System.out.println("\nSegunda Prueba\nLista Enlazada\n");
+        System.out.println("\nLista Enlazada\n");
         ListaEnlazada lista = new ListaEnlazada();
-        System.out.println("Esta vacia: "+ lista.estaVacia());
-        
-        lista.addPrimero("Alex");
-        lista.addPrimero(20);
-        lista.addPrimero("Hello World");
-        lista.addPrimero(3.14);
-        
-        System.out.println("Primer Elemento: "+ lista.obtener(0));
-        System.out.println("Ultimo: "+ lista.obtener(lista.size()-1));
-        System.out.println("Index 2: "+ lista.obtener(2));
-        
-        System.out.println("Esta vacia: "+ lista.estaVacia());
-        System.out.println("Tamanio: "+ lista.size());
-        
+
+        lista.addPrimero(6);
+        lista.addPrimero(5);
+        lista.addPrimero(4);
+        lista.addPrimero(3);
+        lista.addPrimero(2);
+        lista.addPrimero(1);
+
+        lista.cortar(3);
+
+        System.out.println("Primer Elemento: " + lista.obtener(0));
+        System.out.println("Ultimo: " + lista.obtener(lista.size() - 1));
+
+        System.out.println("Tamanio: " + lista.size());
+
     }
 }
